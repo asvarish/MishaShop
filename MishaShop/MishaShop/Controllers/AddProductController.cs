174fd161e2404f39a8d7cf3a77df6cbf05e50ce0
asvarish <42956531+asvarish@users.noreply.github.com>
2020-03-25 21:49:45 +0300

    op-pa

---
+++ MishaShop/MishaShop/Controllers/AddProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MishaShop.Controllers
+{
+    public class AddProductController : Controller
+    {
+        public AddProductController()
+        {
+        }
+
+        [HttpGet]
+        public IActionResult AddProductPage()
+        {
+            return View();
+        }
+    }
+}
+
