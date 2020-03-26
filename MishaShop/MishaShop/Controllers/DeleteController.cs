7aaa8f0eed1d5fd85d773b995b18aa17fe175890
asvarish <42956531+asvarish@users.noreply.github.com>
2020-03-27 00:59:11 +0300

    last

---
+++ MishaShop/MishaShop/Controllers/DeleteController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+//namespace MishaShop.Controllers
+//{
+//    public class DeleteController : Controller
+//    {
+//        public DeleteController()
+//        {
+//        }
+//        [HttpGet]
+//        public async Task<IActionResult> ConfirmDelete()
+//        {
+//            await 
+//        }
+//        [HttpPost]
+//        public IActionResult Delet()
+//        {
+//            return View("CatalogPage");
+//        }
+//    }
+//}
+
