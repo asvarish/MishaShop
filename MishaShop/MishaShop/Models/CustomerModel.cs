f3eebb9f7ab4ea1c4b7b8f1d4a9732d8beffbf7f
asvarish <42956531+asvarish@users.noreply.github.com>
2020-03-27 13:27:14 +0300

    qwe

--- MishaShop/MishaShop/Models/CustomerModel.cs
+++ MishaShop/MishaShop/Models/CustomerModel.cs
@@ -5,17 +5,17 @@ namespace MishaShop.Models
     public class CustomerModel
     {
         public static object Users { get; internal set; }
-        [Required]
+        [Required(ErrorMessage = "Поле должно быть установлено")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Поле должно быть установлено")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Поле должно быть установлено")]
         public string FirstName { get; set; }
         [Required]
         public string LastName { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Поле должно быть установлено")]
         [DataType(DataType.PhoneNumber)]
         public string MobilePhone { get; set; }
     }
